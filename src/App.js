a1ff589a9f3e4f8a6deb2d2e833367b486d071e9
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-07-25 16:10:03 -0400

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Nav from './components/Nav';
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Nav />
     </div>
   );
 }
