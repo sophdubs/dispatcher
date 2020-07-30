34237e9294f8ed475bb9a34287580421f865aaa4
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-07-30 09:58:18 -0400

    Clean up Day.js

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from "../GlobalContext";
-import { generateDaySchedule, fetchDayTasksForDriver, generateCompatibleEndTimes, showNewTaskForm, fetchSelectedTask } from "../helpers/helpers"
+import { generateDaySchedule, fetchDayTasksForDriver, fetchSelectedTask } from "../helpers/helpers"
 import $ from 'jquery';
 
 
@@ -8,7 +8,6 @@ export default function Day(props) {
   const { state, setState } = useContext(GlobalContext);
 
   const onClickTimeSlot = (e) => {
-    console.log(e.target);
     // Grab selected time slot
     const selectedTimeSlot = parseInt(e.target.dataset.time);
     // Set day && Set selected timeslot 
@@ -17,16 +16,21 @@ export default function Day(props) {
 
   const onClickTask = (e) => {
     let taskElem = e.target;
+    // catch event on LI
     while(taskElem.nodeName !== 'LI') {
       taskElem = $(taskElem).parent()[0];
     }
+    // Grab data fields needed from clicked task
     const selectedTimeSlot = taskElem.dataset.start;
     const day = props.day;
+    // Update state with selected task details
     const selectedTask = fetchSelectedTask(day, state.week, state.driver, selectedTimeSlot, state);
     setState({...state, selectedTimeSlot, day, selectedTask});
   }
 
+  // Fetches all the task for a specified driver on a specified week and day
   const tasks = fetchDayTasksForDriver(state.schedule, state.week, props.day, state.driver);
+  // Generates the column for each day of the week
   const daySchedule = generateDaySchedule(tasks, onClickTimeSlot, onClickTask);
   
   return (
