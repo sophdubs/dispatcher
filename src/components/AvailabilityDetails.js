223a5d811be48aa5abdfd4d341b06e290d82b2c8
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-07-30 09:44:03 -0400

    Clean up AvailabilityDetails.js

--- src/components/AvailabilityDetails.js
+++ src/components/AvailabilityDetails.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../GlobalContext";
 import { generateDriverDetails } from "../helpers/helpers";
-import $ from 'jquery';
 
 export default function AvailabilityDetails() {
   const { state, setState } = useContext(GlobalContext); 
