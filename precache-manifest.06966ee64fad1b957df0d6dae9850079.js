5267e464f8a5b07527c8d030cc588941274b7721
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-07-30 11:09:56 -0400

    Updates

---
+++ precache-manifest.06966ee64fad1b957df0d6dae9850079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ddc698fa293c023fe6fb5276d6d0686",
+    "url": "/dispatcher/index.html"
+  },
+  {
+    "revision": "2c27b0a92a85c169683c",
+    "url": "/dispatcher/static/css/main.0845ff2f.chunk.css"
+  },
+  {
+    "revision": "f4ff0770af883b631ec1",
+    "url": "/dispatcher/static/js/2.b0820f03.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/dispatcher/static/js/2.b0820f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c27b0a92a85c169683c",
+    "url": "/dispatcher/static/js/main.69eb4623.chunk.js"
+  },
+  {
+    "revision": "29770efb0dc49cf101e8",
+    "url": "/dispatcher/static/js/runtime-main.f376afee.js"
+  }
+]);
